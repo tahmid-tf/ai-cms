bd78369a005fb8dbedadd2cffda03b78730167fc
Tahmid Ferdous <tahmid.tf1@gmail.com>
2026-03-25 15:05:41 +0600

    nav "Active link" implemented

--- resources/views/components/sidebar/sidebar.blade.php
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -1,19 +1,28 @@
 @role('admin')
     <!-- Sidenav Menu Heading (Core)-->
     <div class="sidenav-menu-heading">Core</div>
+
     <!-- Sidenav Accordion (Dashboard)-->
     <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseDashboards"
-        aria-expanded="false" aria-controls="collapseDashboards">
+        aria-expanded="{{ request()->routeIs('admin.users.*') ? 'true' : 'false' }}" aria-controls="collapseDashboards">
         <div class="nav-link-icon"><i data-feather="activity"></i></div>
         Users
         <div class="sidenav-collapse-arrow">
             <i class="fas fa-angle-down"></i>
         </div>
     </a>
-    <div class="collapse" id="collapseDashboards" data-bs-parent="#accordionSidenav">
+
+    <div class="collapse {{ request()->routeIs('admin.users.*') ? 'show' : '' }}" id="collapseDashboards"
+        data-bs-parent="#accordionSidenav">
         <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
-            <a class="nav-link" href="{{ route('admin.users.create') }}">Create User</a>
-            <a class="nav-link" href="{{ route('admin.users.index') }}">View Users</a>
+            <a class="nav-link {{ request()->routeIs('admin.users.create') ? 'active' : '' }}"
+                href="{{ route('admin.users.create') }}">
+                Create User
+            </a>
+            <a class="nav-link {{ request()->routeIs('admin.users.index') ? 'active' : '' }}"
+                href="{{ route('admin.users.index') }}">
+                View Users
+            </a>
         </nav>
     </div>
 @endrole
