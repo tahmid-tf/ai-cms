abfa14d15164db0aabd3ab37c445e7e345b018c4
Tahmid Ferdous <tahmid.tf1@gmail.com>
2026-03-25 15:08:47 +0600

    Route file and route resource modification

--- routes/admin.php
+++ routes/admin.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::resource('users', UserController::class);
+    });
+
+    
--- routes/web.php
+++ routes/web.php
@@ -47,17 +47,7 @@ Route::middleware(['auth', 'role:viewer'])->group(function () {
 
 // --------------------------- Admin Dashboards ---------------------------
 
-Route::middleware(['auth'])
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
-        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-        Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-        Route::get('/users', [UserController::class, 'index'])->name('users.index');
-        Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-        Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-        Route::delete('/users/{id}', [UserController::class, 'destroy']);
-    });
+
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
